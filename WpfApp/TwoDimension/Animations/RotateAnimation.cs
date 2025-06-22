fc90f29a8251b1da6a987faf5c836d720c692c82
KaneV <votrungkien05032002@gmail.com>
2025-06-22 11:12:44 +0700

    adjust animation logic

--- WpfApp/TwoDimension/Animations/RotateAnimation.cs
+++ WpfApp/TwoDimension/Animations/RotateAnimation.cs
@@ -9,17 +9,20 @@ public class RotateAnimation : Animation2D
 {
     private double _totalAngle; // Total angle in degrees
     private double _anglePerMillisecond; // Pre-calculated angle increment per millisecond
+    private Func<Point> _centerOfRotationSource; // Center around which to rotate
 
-    public RotateAnimation(ShapeContainer shape, double totalAngle, TimeSpan duration) : base(shape, duration)
+    public RotateAnimation(ShapeContainer shape, double totalAngle, TimeSpan duration, Func<Point> centerOfRotationSource) : base(shape, duration)
     {
         _totalAngle = totalAngle;
         _anglePerMillisecond = totalAngle / duration.TotalMilliseconds;
+        _centerOfRotationSource = centerOfRotationSource ?? throw new ArgumentNullException(nameof(centerOfRotationSource));
     }
 
     protected override void ApplyTransformation(double progress, TimeSpan deltaTime)
     {
         // Apply the delta rotation for this time step around the specified center
-        Shape.TransformShape(p => TransformationMatrix.Rotate(p,
+        var centerOfRotation = _centerOfRotationSource();
+        Shape.TransformShape(p => TransformationMatrix.RotateAround(p, centerOfRotation,
             _anglePerMillisecond * deltaTime.TotalMilliseconds));
     }
 }
